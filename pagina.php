97db1cc1180ae2399607f8f60af8b4b4e2622fba
Pablo Machado <pmmachadov@gmail.com>
2022-12-01 15:10:06 +0100

    Table updated.

--- pagina.php
+++ pagina.php
@@ -12,13 +12,31 @@
     <!-- Crea una función sumatorio() que requiera un parámetro de entada y devuelva el
 sumatorio de todos los valores que se le van pasando con cada invocación de la
 función.  -->
+<?php
+include 'funlib.php';
+echo "<table border = 1>";
+echo "<tr>";
+echo "  <td> Pablo  </td>";
+echo "  <td> Machado </td>";
+echo "</tr>";
+echo "<td> " . "Ejercicio" . "</td>";
+echo "<td> " . "anexo2-pag71-ej5" . "</td>";
+echo "</tr>";
+echo "<td> " . "    echo sumatorio(10);
+    echo sumatorio(20);
+    echo sumatorio(30);
+    echo sumatorio(40);
+    echo sumatorio(50);" . "</td>";
+echo "<td> " . "10
+    30
+    60
+    100
+    150" . "</td>";
+echo "</tr>";
+echo "</table>";
+?>
     <?php
-    function sumatorio($num)
-    {
-        static $suma = 0;
-        $suma += $num;
-        return $suma;
-    }
+
     echo sumatorio(10) . "<br>";
     echo sumatorio(20) . "<br>";
     echo sumatorio(30) . "<br>";
